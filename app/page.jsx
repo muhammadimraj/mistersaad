1cf47a301283b52ec2052cf42e8b3f18ac20c896
muhammadimraj <muhammadimraj007@gmail.com>
2025-10-30 10:55:21 +0500

    change the title of the project

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,9 @@ import Home from "./(dark-homes)/index-01/page";
 
 export const metadata = {
   title:
-    "Home 01 || Personal Portfolio React Nextjs Template | Freelancer & Developer Portfolio",
+    "Mr. Saad | Expert Mobile App Developer Dubai",
   description:
-    "Personal Portfolio React Nextjs Template | Freelancer & Developer Portfolio",
+    "Mr. Saad is an expert mobile app developer in Dubai. He has been helping companies in Dubai, Abu Dhabi, and worldwide for over 20 years. He is interested in developing and enhancing mobile apps for both iOS and Android platforms.",
 };
 export default function page() {
   return (
